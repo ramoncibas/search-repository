6a24b32f4e7e9e79b9e636a999c396eec433d9f1
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-01 19:08:57 -0300

    Update Main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,101 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, View, Image, Text, StyleSheet, Linking } from 'react-native'
-import { Feather } from '@expo/vector-icons'
-
+import React, { useEffect, useState } from "react";
+import { FlatList, View, Image, Text, StyleSheet, Linking } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 interface Repository {
-    name: string;
-    full_name : string;
-    private: boolean;
-    html_url: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  html_url: string;
 
-    // User information
-    owner: { login: string, avatar_url: string };
+  // User information
+  owner: { login: string; avatar_url: string };
 }
 
-const Main: React.FC = () => {
-    const [repositories, setRepos] = useState<Repository[]>([]);
+const Main = (props: any) => {
+  const [repositories, setRepos] = useState<Repository[]>([]);
+  console.log(props);
 
-    useEffect(() => {
-        fetch('https://api.github.com/users/ramoncibas/repos').then(response => {
-            response.json().then(data => {
-                setRepos(data);                
-            })
-        })
-    }, []);
-
-    return (
-        <FlatList 
-            contentContainerStyle={{ padding: 24 }}
-            data={  repositories }
-            keyExtractor={ repos => repos.name }
-            ListHeaderComponent={() => {
-                return(
-                    <View style={styles.flatHeader}>
-                        <Text style={{ marginRight: 10 }}>Veja seus repositorios no Github</Text>
-                        <Feather name="github" size={20}/>        
-                    </View>
-                )
-            }}
-            renderItem={({ item: repos }) => (
-                <View style={ styles.repository }>
-                    <Image style={ styles.image } source={{ uri: repos.owner.avatar_url }} />
-                    <Text style={ styles.textRepos }>Nome do usuario: { repos.owner.login }</Text>
-                    <Text style={ styles.textRepos }>Path: { repos.full_name }</Text>
-                    { 
-                        repos.private == false 
-                        ? <Text style={ styles.textRepos }> Repositorio Publico </Text> 
-                        : <Text style={ styles.textRepos }> Repositorio Privado </Text>
-                    }
-
-                    <Text style={ styles.link } onPress={() => Linking.openURL(repos.html_url)}>
-                        Repositório
-                    </Text>
-                </View>
-            )}
-        />
+  useEffect(() => {
+    fetch("https://api.github.com/users/" + props.user + "/repos").then(
+      (response) => {
+        response.json().then((data) => {
+          setRepos(data);
+        });
+      }
     );
-}
+  }, []);
+
+  return (
+    <FlatList
+      contentContainerStyle={{ padding: 24 }}
+      data={repositories}
+      keyExtractor={(repos) => repos.name}
+      ListHeaderComponent={() => {
+        return (
+          <View style={styles.flatHeader}>
+            <Text style={{ marginRight: 10, fontSize: 16 }}>
+              Veja seus repositorios no Github
+            </Text>
+            <Feather name="github" size={20} />
+          </View>
+        );
+      }}
+      renderItem={({ item: repos }) => (
+        <View style={styles.repository}>
+          <Image
+            style={styles.image}
+            source={{ uri: repos.owner.avatar_url }}
+          />
+          <Text style={styles.textRepos}>
+            Nome do usuario: {repos.owner.login}
+          </Text>
+          <Text style={styles.textRepos}>Path: {repos.full_name}</Text>
+          {repos.private == false ? (
+            <Text style={styles.textRepos}> Repositorio Publico </Text>
+          ) : (
+            <Text style={styles.textRepos}> Repositorio Privado </Text>
+          )}
+
+          <Text
+            style={styles.link}
+            onPress={() => Linking.openURL(repos.html_url)}
+          >
+            Repositório
+          </Text>
+        </View>
+      )}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    flatHeader: {
-        width : '100%',
-        height: 100,
-        marginBottom: '2rem',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-        borderBottomColor: '#000',
-        borderBottomWidth: 1
-    },
-    repository: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 30,
-        backgroundColor: '#0d1117',        
-        borderRadius: 10,
-        padding: 10
-    },
-    image: {
-        width: 48,
-        height: 48,
-        borderRadius: 24,
-        marginRight: 16,
-    },
-    link: {
-        marginTop: 10,
-        padding: 10,
-        width: '50%',
-        borderRadius: 10,
-        backgroundColor: '#20232a',
-        color: '#fff',
-        textAlign: 'center',
-    },
-    textRepos: {
-        marginTop: 10,
-        color: '#fff',
-    }
-})
+  flatHeader: {
+    width: "100%",
+    height: 100,
+    marginBottom: "2rem",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+  },
+  repository: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 30,
+    backgroundColor: "#0d1117",
+    borderRadius: 10,
+    padding: 10,
+  },
+  image: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    marginRight: 16,
+  },
+  link: {
+    marginTop: 10,
+    padding: 10,
+    width: "50%",
+    borderRadius: 10,
+    backgroundColor: "#20232a",
+    color: "#fff",
+    textAlign: "center",
+  },
+  textRepos: {
+    marginTop: 10,
+    color: "#fff",
+  },
+});
 
-export default Main
+export default Main;
